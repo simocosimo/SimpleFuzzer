22242fa4b5bb55ceed04e988ca28286f1bbe4af3
Simone Cosimo <simosx4@gmail.com>
2021-07-28 23:18:56 +0200

    Implemented execution of the fuzzing on target

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <random>
 #include <bitset>
+#include <array>
+#include <memory>
 
 std::vector<std::bitset<8>> get_data(const std::string filename) {
     
@@ -11,7 +13,7 @@ std::vector<std::bitset<8>> get_data(const std::string filename) {
     if(file.is_open()) {
         std::vector<std::bitset<8>> data(std::istreambuf_iterator<char>(file), {});
 
-        std::cout << "File '" << filename << "' read correctly" << std::endl;
+        //std::cout << "File '" << filename << "' read correctly" << std::endl;
         file.close();
         return data;
     }
@@ -28,7 +30,7 @@ bool create_new(const std::vector<std::bitset<8>> data, const std::string filena
         for(auto d : data) cc.push_back(static_cast<unsigned char>(d.to_ulong()));
         file.write(static_cast<const char*>(&cc[0]), cc.size());
         file.close();
-        std::cout << "Data written to '" << filename << "' correctly" << std::endl;
+        //std::cout << "Data written to '" << filename << "' correctly" << std::endl;
         return true;
     }
 
@@ -53,18 +55,18 @@ void bit_flip(std::vector<std::bitset<8>> &data) {
         chosen_indexes.push_back(random(static_cast<unsigned long>(4), data.size() - 4));
     }
 
-    std::cout << "Number of chosen indexes: " << chosen_indexes.size() << std::endl;
-    std::cout << "Chosen indexes: ";
-    for(auto i = chosen_indexes.begin(); i != chosen_indexes.end(); ++i) {
-        std::cout << *i << " ";
-    }
-    std::cout << std::endl;
+    //std::cout << "Number of chosen indexes: " << chosen_indexes.size() << std::endl;
+    //std::cout << "Chosen indexes: ";
+    // for(auto i = chosen_indexes.begin(); i != chosen_indexes.end(); ++i) {
+    //     std::cout << *i << " ";
+    // }
+    //std::cout << std::endl;
 
     for(auto i : chosen_indexes) {
         auto rand_bit = random(0, 7);
         auto prev = data[i];
         data[i].flip(rand_bit);
-        std::cout << "data[" << i << "] changed from " << prev << " to " << data[i] << std::endl;
+        //std::cout << "data[" << i << "] changed from " << prev << " to " << data[i] << std::endl;
     }
 
 }
@@ -90,7 +92,7 @@ void magic(std::vector<std::bitset<8>> &data) {
     int picked_magic = random(0, 9);
     int chosen_index = random(static_cast<unsigned long>(4), data.size() - 4);
     std::bitset<4*8> bitmask(0xFF);
-    std::cout << "Picked: " << picked_magic << ", " << magics[picked_magic] << std::endl;
+    //std::cout << "Picked: " << picked_magic << ", " << magics[picked_magic] << std::endl;
 
     if (picked_magic >= 0 && picked_magic <=2) {
         // One byte values
@@ -102,7 +104,7 @@ void magic(std::vector<std::bitset<8>> &data) {
         data[chosen_index] = higher;
         data[chosen_index + 1] = lower;
 
-        std::cout << lower << " " << higher << std::endl;
+        //std::cout << lower << " " << higher << std::endl;
     } else {
         // Four bytes values
         std::bitset<8> low_lower((magics[picked_magic] & bitmask).to_ullong());
@@ -114,7 +116,44 @@ void magic(std::vector<std::bitset<8>> &data) {
         data[chosen_index + 2] = lower;
         data[chosen_index + 3] = low_lower;
 
-        std::cout << low_lower << " " << lower << " " << higher << " " << high_higher << std::endl; 
+        //std::cout << low_lower << " " << lower << " " << higher << " " << high_higher << std::endl; 
+    }
+}
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+
+    auto pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    
+    while (!feof(pipe)) {
+        if (fgets(buffer.data(), 128, pipe) != nullptr)
+            result += buffer.data();
+    }
+    
+    pclose(pipe);
+    return result;
+}
+
+void exif(int counter, const std::vector<std::bitset<8>> data) {
+
+    std::string command = "bash -c \"{ ./exif/exif modded.jpg -verbose; } 2>&1\"";
+    std::string output = exec(command.c_str());
+
+    if(output.find("Segmentation") != std::string::npos) {
+        // Segmentation fault founded
+        char buffer[50];
+        sprintf(buffer, "crashes/crash.%d.jpg", counter);
+        if(!create_new(data, buffer)) {
+            exit(1);
+        }
+
+        std::cout << "Found a SIGSEGV on iteration #" << counter << std::endl;
+    }
+
+    if (counter % 100 == 0) {
+        std::cout << "Iteration #" << counter << std::endl;
     }
 }
 
@@ -126,21 +165,29 @@ int main(int argc, char** argv) {
         std::cout << "Usage: " << argv[0] << " <valid_jpg>" << std::endl;
         exit(1);
     }
-
-    const std::string filename = argv[1];
-    auto data = get_data(filename);
-
-    // TODO: bettere error handling
-    if(data.size() == 0) {
-        exit(1);
-    }
     
-    // -- Bit flipping
-    // bit_flip(data);
-    magic(data);
+    const std::string filename = argv[1];
 
-    if(!create_new(data, output_file)) {
-        exit(1);
+    for(int i = 0; i < 1000; ++i) {
+        auto data = get_data(filename);
+
+        // TODO: better error handling
+        if(data.size() == 0) {
+            exit(1);
+        }
+        
+        int fuzz_mode = random(0, 1);
+        if (fuzz_mode) {
+            bit_flip(data);
+        } else {
+            magic(data);
+        }
+
+        if(!create_new(data, output_file)) {
+            exit(1);
+        }
+
+        exif(i, data);
     }
 
     return 0;
